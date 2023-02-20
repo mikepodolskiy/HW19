6bc4b266bf832e24ff3ed51fa16b20b4f43d5097
mikepodolskiy <mikepodolskiy@gmail.com>
2023-02-20 11:20:55 +0300

    user model updated - unique user condition (unique = True)

--- dao/model/user.py
+++ dao/model/user.py
@@ -7,9 +7,10 @@ from setup_db import db
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-    role = db.Column(db.String)
+    username = db.Column(db.String(100), unique=True)  # unique parameter provision, user with same name will not be
+    # created
+    password = db.Column(db.String(50))
+    role = db.Column(db.String(50))
 
 
 # creating Schema class as inheritance of Schema for serialization
